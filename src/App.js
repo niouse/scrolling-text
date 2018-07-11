cd59a28dcb2a9955c0fbfed02299062075655fb7
Niouse <seba.pinard@gmail.com>
2018-07-11 19:08:20 +0200

    first step

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+import bracket from './crochet.png'
 
-const Container = (props) => <div>{props.children}</div>
-const Img = (props) => <img src={props.src} alt={props.alt} />
-const FixedWrapper = (props) => <div> {props.children} </div>
-const MobileWrapper = (props) => <div> {props.children} </div>
+
+const containerStyles = {
+  display : "flex",
+  //alignItems : "center",
+  //border : "1px solid pink"
+}
+
+const fixedWrapperStyles = {
+  height : "30px",
+  position : "relative",
+  overflow : "hidden",
+  //border : "1px solid blue"
+}
+
+const mobileWrapperStyles = {
+  position : "relative",
+  top : "0px",
+  //border : "1px solid red"
+}
+
+const imgStyles = (rotated) => ({
+  height : "30px",
+  width : "auto",
+  transform : rotated ? 'rotate(180deg)' : 'none',
+})
+
+const textStyles = {
+  height : "30px",
+  display : "flex",
+  alignItems : "center",
+  justifyContent : "center",
+  fontSize : "20px",
+  //border : "1px solid yellow"
+}
+
+const Container = ({children}) => <span style={containerStyles}> {children} </span>
+const Img = ({src, alt, rotated}) => <img style={imgStyles(rotated)} src={src} alt={alt} />
+const FixedWrapper = ({children}) => <div style={fixedWrapperStyles}> {children} </div>
+const MobileWrapper = ({children}) => <div style={mobileWrapperStyles}> {children} </div>
+const Text = ({children}) => <div style={textStyles}> {children} </div>
 
 class App extends Component {
   render() {
     return (
       <Container>
-        <Img />
+        <Img src={bracket} alt='#'/>
         <FixedWrapper>
           <MobileWrapper>
             {this.props.texts.map((item, index) => (
-              <div>{item}</div>
+              <Text key={'scrolling-text'+index}>{item}</Text>
             ))}
           </MobileWrapper>
         </FixedWrapper>
-        <Img />
+        <Img src={bracket} alt='#' rotated/>
       </Container>
     );
   }
